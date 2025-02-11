546ab8537be71336d037f50f3ecfb5c1d88832c8
“alerenna” <alessandrorennafm@gmail.com>
2025-02-11 20:32:39 +0800

    Add operations list

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+/* 
+OPERAZIONI:
+
+- dichiarare variabile numero
+- creare loop per numeri da 1 a 100
+- la condizione deve stampare i numeri da uno e minori uguali a 100
+- la variabile del loop deve essere incrementale
+- creare una funzione condizionale che presupponga che:
+    - SE il numero stampato è divisibile per 3 allora stampi variabile Fizz
+    - Altrimenti Se il numero stampato è divisibile per 5 stampi variabile Buzz
+    - Altrimenti stampi il numero
+*/
